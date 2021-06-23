f348dec4cc8a3d198e5e21e71db2df7da1170935
Jahongir Nagmatov <jahongirnagmatov@gmail.com>
2021-06-23 12:55:10 +0500

    update TextFeature (add texts and headings)

--- src/components/text-feature.js
+++ src/components/text-feature.js
@@ -9,7 +9,29 @@ export default function TextFeature({
   btnURL = '#',
 }) {
   return (
-    <h1>TextFeature</h1>
+    <Box sx={styles.Card}>
+      <Box sx={styles.wrapper}>
+        <Text as="p" sx={styles.wrapper.subTitle}>
+          {subTitle}
+        </Text>
+        <Heading as="h2" sx={styles.wrapper.title}>
+          {title} 
+        </Heading>
+      </Box>
+      {description && (
+        <Text as="p" className="description" sx={styles.description}>
+          {description}
+        </Text>
+      )} /* if description is true it will do the function below the description */
+        {btnName &&  (
+          <Link href={btnURL} variant="default">
+            <Button variant="primary" aria-label="{btnName}">
+              {btnName}
+            </Button>
+          </Link>
+        )}
+
+    </Box>
   );
 }
 
